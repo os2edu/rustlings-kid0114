5ae5363db2d004d20323550b4b49ec0cd4463480
kid0114 <kidyln114@gmail.com>
2022-11-07 04:47:57 +0800

    intro change

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -2,7 +2,7 @@
 // Make the code print a greeting to the world.
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a hint.
 
-
+//
 
 fn main() {
     println!("Hello {{}}!");
